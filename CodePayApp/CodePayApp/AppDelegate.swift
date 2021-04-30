eaaa4fedded14dd6c47e32492de7deaf23c92b01
TonyJem <a.jemanov@gmail.com>
2021-04-30 21:22:30 +0300

    Add Login View controller embeded in Navigation Controller

--- CodePayApp/CodePayApp/AppDelegate.swift
+++ CodePayApp/CodePayApp/AppDelegate.swift
@@ -1,14 +1,10 @@
 import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow()
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+        // Override point for customization after application launch.
         return true
     }
     
