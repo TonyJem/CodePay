8a7079dfff68551dc140c78e5aa88d687b8c533d
TonyJem <a.jemanov@gmail.com>
2021-05-01 11:12:08 +0300

    Update LoginViewController+Constraints.swift

--- CodePayApp/CodePayApp/Login/LoginViewController+Constraints.swift
+++ CodePayApp/CodePayApp/Login/LoginViewController+Constraints.swift
@@ -4,8 +4,27 @@ import SnapKit
 extension LoginViewController {
     func setupConstraints() {
         personView.snp.makeConstraints { make in
-            make.top.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
-            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(90)
+            make.width.equalTo(90)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(50)
+            make.centerX.equalToSuperview()
         }
     }
 }
+
+// MARK:  - SnapKit Constraints Examples
+/*
+ make.width.equalToSuperview().multipliedBy(0.45)
+ 
+ make.height.equalTo(45)
+ 
+ make.width.equalTo(90)
+ 
+ make.top.equalTo(viewProgress.snp.bottom).offset(32)
+ 
+ make.centerX.equalToSuperview()
+ 
+ make.top.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+ 
+ make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+ */
