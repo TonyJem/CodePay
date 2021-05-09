694b81a2e8891f65ac15db54177319945531c5e8
TonyJem <a.jemanov@gmail.com>
2021-05-09 14:38:12 +0300

    Clean project to Start version2

--- CodePayApp/CodePayApp/Delegate/SceneDelegate.swift
+++ CodePayApp/CodePayApp/Delegate/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let navController = UINavigationController()
-        let viewController = LoginRegisterVC()
+        let viewController = LoginVC()
         navController.viewControllers = [viewController]
         navController.navigationBar.barTintColor = Colors.mainBackground
         
