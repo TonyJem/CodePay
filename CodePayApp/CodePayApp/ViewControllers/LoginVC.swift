2f3e414b986ac6481ed7f761e0b484b9d36720a3
TonyJem <a.jemanov@gmail.com>
2021-05-09 15:02:31 +0300

    Add personImageContainer to Login

--- CodePayApp/CodePayApp/ViewControllers/LoginVC.swift
+++ CodePayApp/CodePayApp/ViewControllers/LoginVC.swift
@@ -1,10 +1,31 @@
 import UIKit
+import SnapKit
 
 class LoginVC: UIViewController {
+    
+    lazy var personImageContainer: UIView = {
+       let view = UIView()
+        
+        view.backgroundColor = .systemPink
+        
+        self.view.addSubview(view)
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = Colors.mainBackground
+        setupConstraints()
+    }
+}
+
+private extension LoginVC {
+    private func setupConstraints() {
+        personImageContainer.snp.makeConstraints { make in
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+            make.centerX.centerY.equalToSuperview()
+        }
     }
 }
