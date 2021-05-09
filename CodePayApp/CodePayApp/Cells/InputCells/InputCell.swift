695b96b38ad2081e9c7ce0e5816048395a1d2dde
TonyJem <a.jemanov@gmail.com>
2021-05-09 11:14:44 +0300

    Migrate InputView to inputCell

--- CodePayApp/CodePayApp/Cells/InputCells/InputCell.swift
+++ CodePayApp/CodePayApp/Cells/InputCells/InputCell.swift
@@ -2,29 +2,93 @@ import UIKit
 
 class InputCell: BaseTableViewCell {
     
-    private let inputCellHeight: CGFloat = 70
+    private let edgeInset: CGFloat = 10
     
-    private let inputContainer: InputView = {
-        let view = InputView()
+    lazy var containerView: UIView = {
+        let view = UIView()
+        
+        self.addSubview(view)
+        return view
+    }()
+    
+    lazy var titleLabel: CodePayLabel = {
+        let lbl = CodePayLabel(frame: .zero)
+        lbl.setup(title: inputType().title, dto: CodePayLabelDTO.title)
+        
+        containerView.addSubview(lbl)
+        return lbl
+    }()
+    
+    lazy var iconContainerView: UIView = {
+        let view = UIView()
+        
+        containerView.addSubview(view)
+        return view
+    }()
+    
+    lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(imageLiteralResourceName: inputType().imageName))
+        
+        iconContainerView.addSubview(imageView)
+        return imageView
+    }()
+    
+    lazy var inputContainer: TextFieldContainer = {
+        let container = inputType().container
+        container.setPlaceHolder(placeholder: inputType().placeholder)
+        
+        containerView.addSubview(container)
+        return container
+    }()
+    
+    lazy var bottomLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Colors.lightBlue
+        
+        containerView.addSubview(view)
         return view
     }()
     
     override func setupView() {
         super.setupView()
-        
-        inputContainer.type = inputType()
-        contentView.addSubview(inputContainer)
     }
     
     override func setupConstraints() {
         super.setupConstraints()
         
-        inputContainer.snp.makeConstraints { make in
-            make.width.equalTo(Core.elementWidth)
-            make.height.equalTo(inputCellHeight)
-            make.centerX.centerY.equalTo(contentView)
+        containerView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(edgeInset)
+            make.leading.trailing.equalToSuperview()
         }
-        inputContainer.setupConstraints()
+        
+        titleLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.leading.equalToSuperview()
+        }
+        
+        bottomLineView.snp.makeConstraints { make in
+            make.height.equalTo(3)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        iconContainerView.snp.makeConstraints { make in
+            make.width.equalTo(35)
+            make.height.equalTo(35)
+            make.leading.equalToSuperview()
+            make.bottom.equalTo(bottomLineView.snp.top).offset(-4)
+        }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.edges.equalTo(iconContainerView)
+        }
+        
+        inputContainer.snp.makeConstraints { make in
+            make.height.equalTo(35)
+            make.leading.equalTo(iconContainerView.snp.trailing).offset(10)
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(bottomLineView.snp.top).inset(-4)
+        }
+        inputContainer.setupContainerConstraints()
     }
     
     func inputType() -> InputViewType {
