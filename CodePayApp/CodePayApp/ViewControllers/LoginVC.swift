f5b441489e87927a6ef4d39560f7117665226a48
TonyJem <a.jemanov@gmail.com>
2021-05-09 15:07:57 +0300

    Add personImage to LoginUI

--- CodePayApp/CodePayApp/ViewControllers/LoginVC.swift
+++ CodePayApp/CodePayApp/ViewControllers/LoginVC.swift
@@ -3,15 +3,27 @@ import SnapKit
 
 class LoginVC: UIViewController {
     
+    private let imageSizeMultiplier: CGFloat = 0.8888
+    
     lazy var personImageContainer: UIView = {
-       let view = UIView()
+        let view = UIView()
         
-        view.backgroundColor = .systemPink
+        view.backgroundColor = Colors.lightBlue
+        
+        view.layer.cornerRadius = 45
         
         self.view.addSubview(view)
         return view
     }()
     
+    lazy var personImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage(imageLiteralResourceName: "person"))
+        
+        personImageContainer.addSubview(imageView)
+        return imageView
+    }()
+    
+    // MARK:  - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,12 +32,19 @@ class LoginVC: UIViewController {
     }
 }
 
+// MARK:  - LoginVC constraints
 private extension LoginVC {
     private func setupConstraints() {
         personImageContainer.snp.makeConstraints { make in
-            make.height.equalTo(100)
-            make.width.equalTo(100)
+            make.height.equalTo(90)
+            make.width.equalTo(90)
             make.centerX.centerY.equalToSuperview()
+        }
+        
+        personImage.snp.makeConstraints { make in
+            make.width.height.equalToSuperview().multipliedBy(imageSizeMultiplier)
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
         }
     }
 }
