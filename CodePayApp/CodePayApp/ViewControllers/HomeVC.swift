227bde4c8c4814e8217fc79c4f8b88b7938d48ff
TonyJem <a.jemanov@gmail.com>
2021-05-11 22:32:49 +0300

    Update HomeVC.swift

--- CodePayApp/CodePayApp/ViewControllers/HomeVC.swift
+++ CodePayApp/CodePayApp/ViewControllers/HomeVC.swift
@@ -2,9 +2,36 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    lazy var submitButton: CodePayButton = {
+        let btn = CodePayButton(type: .custom)
+        btn.showsTouchWhenHighlighted = true
+        btn.addTarget(self, action: #selector(submitButtonDidTap(_:)), for: .touchUpInside)
+        btn.setup(title: "Home Screen", dto: CodePayButtonDTO.submit)
+        
+        view.addSubview(btn)
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = Colors.mainBackground
+        setupConstraints()
+    }
+    
+    // MARK:  - Actions
+    @objc func submitButtonDidTap(_ sender: UIButton) {
+        print("🟢 'Home' button in Login Scene did Tap")
+    }
+}
+// MARK:  - LoginVC constraints
+private extension HomeVC {
+    private func setupConstraints() {
+        submitButton.snp.makeConstraints { make in
+            make.width.equalTo(Core.itemWidth)
+            make.height.equalTo(60)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.centerX.equalToSuperview()
+        }
     }
 }
