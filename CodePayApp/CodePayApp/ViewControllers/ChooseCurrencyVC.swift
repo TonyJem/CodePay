ad49a3c2ffab324ae4f5f4631a976643756abbff
TonyJem <a.jemanov@gmail.com>
2021-05-11 22:19:55 +0300

    Update ChooseCurrencyVC.swift

--- CodePayApp/CodePayApp/ViewControllers/ChooseCurrencyVC.swift
+++ CodePayApp/CodePayApp/ViewControllers/ChooseCurrencyVC.swift
@@ -1,11 +1,41 @@
 import UIKit
 
 class ChooseCurrencyVC: UIViewController {
-    
+
+    private let myArray: NSArray = ["EUR", "USD", "GBP", "RUB", "JPY"]
+    private var myTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = Colors.mainBackground
+        
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+
+        myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight - 100))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        self.view.addSubview(myTableView)
     }
-    
+}
+
+extension ChooseCurrencyVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(myArray[indexPath.row])"
+        return cell
+    }
+}
+
+extension ChooseCurrencyVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(myArray[indexPath.row])")
+    }
 }
