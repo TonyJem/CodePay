aa50b7d7d8a5b0ab7bbae01410bf1b02935aafa2
TonyJem <a.jemanov@gmail.com>
2021-05-14 10:09:39 +0300

    Update CurrencyCell.swift

--- CodePayApp/CodePayApp/CurrencyCell.swift
+++ CodePayApp/CodePayApp/CurrencyCell.swift
@@ -1,30 +1,25 @@
 import UIKit
 
 class CurrencyCell: BaseTableViewCell {
-    private let newReminderTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Title"
-        textField.font = UIFont(name: "HelveticaNeue", size: 15)
-        return textField
-    }()
+    
+    let containerView = UIView()
+   
+    
     
     override func setupView() {
         super.setupView()
         
-        contentView.addSubview(newReminderTextField)
+        contentView.addSubview(containerView)
         
         backgroundColor = .systemGray6
         contentView.backgroundColor = .white
-        contentView.layer.masksToBounds = true
     }
     
     override func setupConstraints() {
         super.setupConstraints()
         
-        newReminderTextField.snp.makeConstraints { make in
-            make.centerY.equalTo(contentView)
-            make.leading.equalTo(contentView).offset(10)
-            make.trailing.equalTo(contentView).inset(10)
+        containerView.snp.makeConstraints { make in
+            make.leading.trailing.top.bottom.equalTo(contentView).inset(10)
         }
     }
 }
