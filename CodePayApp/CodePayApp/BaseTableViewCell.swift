127c62b571cca6798b303bd52d8b015f6539c8f5
TonyJem <a.jemanov@gmail.com>
2021-05-14 09:56:06 +0300

    Start creating CurrencyCell

---
+++ CodePayApp/CodePayApp/BaseTableViewCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class BaseTableViewCell: UITableViewCell, ReusableView {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {}
+    func setupConstraints() {}
+}
