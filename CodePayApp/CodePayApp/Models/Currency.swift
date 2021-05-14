d679fc52bc3252b219bc21f2967c2ea48a3abc88
TonyJem <a.jemanov@gmail.com>
2021-05-14 18:37:12 +0300

    Add more real currencies

--- CodePayApp/CodePayApp/Models/Currency.swift
+++ CodePayApp/CodePayApp/Models/Currency.swift
@@ -14,25 +14,17 @@ struct Currency {
 
 class CurrencyModel {
     
-//    var currencies: [Currency] = [
-//        Currency(isSelected: false, flag: "🇪🇺", acronym: "EUR", symbol: "\u{20AC}", description: "Euro"),
-//        Currency(isSelected: false, flag: "🇺🇸", acronym: "USD", symbol: "\u{FF04}", description: "US dollar"),
-//        Currency(isSelected: false, flag: "🇬🇧", acronym: "GBP", symbol: "\u{00A3}", description: "Pounds sterling"),
-//        Currency(isSelected: false, flag: "🇷🇺", acronym: "RUB", symbol: "\u{20BD}", description: "Russian ruble"),
-//        Currency(isSelected: false, flag: "🇯🇵", acronym: "JPY", symbol: "\u{00A5}", description: "Japanese yen")
-//    ]
-    
     var currencies: [Currency] = [
         Currency(isSelected: false, flag: "🇪🇺", acronym: "EUR", symbol: "\u{20AC}", description: "Euro"),
-        Currency(isSelected: false, flag: "🇺🇸", acronym: "USD", symbol: "\u{FF04}", description: "US dollar"),
-        Currency(isSelected: false, flag: "🇬🇧", acronym: "GBP", symbol: "\u{00A3}", description: "Pounds sterling"),
-        Currency(isSelected: false, flag: "🇷🇺", acronym: "RUB", symbol: "\u{20BD}", description: "Russian ruble"),
-        Currency(isSelected: false, flag: "🇯🇵", acronym: "JPY", symbol: "\u{00A5}", description: "Japanese yen"),
-        Currency(isSelected: false, flag: "🇪🇺", acronym: "EUR", symbol: "\u{20AC}", description: "Euro"),
-        Currency(isSelected: false, flag: "🇺🇸", acronym: "USD", symbol: "\u{FF04}", description: "US dollar"),
-        Currency(isSelected: false, flag: "🇬🇧", acronym: "GBP", symbol: "\u{00A3}", description: "Pounds sterling"),
-        Currency(isSelected: false, flag: "🇷🇺", acronym: "RUB", symbol: "\u{20BD}", description: "Russian ruble"),
-        Currency(isSelected: false, flag: "🇯🇵", acronym: "JPY", symbol: "\u{00A5}", description: "Japanese yen")
+        Currency(isSelected: false, flag: "🇺🇸", acronym: "USD", symbol: "\u{FF04}", description: "US Dollar"),
+        Currency(isSelected: false, flag: "🇬🇧", acronym: "GBP", symbol: "\u{00A3}", description: "Pounds Sterling"),
+        Currency(isSelected: false, flag: "🇷🇺", acronym: "RUB", symbol: "\u{20BD}", description: "Russian Ruble"),
+        Currency(isSelected: false, flag: "🇯🇵", acronym: "JPY", symbol: "\u{00A5}", description: "Japanese Yen"),
+        Currency(isSelected: false, flag: "🇲🇳", acronym: "MNT", symbol: "\u{20AE}", description: "Mongolian Tugrik"),
+        Currency(isSelected: false, flag: "🇺🇦", acronym: "UAH", symbol: "\u{20B4}", description: "Ukrainian Hryvnia"),
+        Currency(isSelected: false, flag: "🇮🇱", acronym: "ILS", symbol: "\u{20AA}", description: "Israeli Shekel"),
+        Currency(isSelected: false, flag: "🇮🇳", acronym: "INR", symbol: "\u{20B9}", description: "Indian Rupee"),
+        Currency(isSelected: false, flag: "🇵🇭", acronym: "PHP", symbol: "\u{20B1}", description: "Philippine Peso")
     ]
     
     func markCurrency(at row: Int) {
