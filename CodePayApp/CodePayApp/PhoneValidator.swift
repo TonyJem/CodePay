6d250e04dc9be9867b6a7a199d56bc8f39a1e83c
TonyJem <a.jemanov@gmail.com>
2021-05-15 18:39:19 +0300

    Update PhoneValidator.swift

--- CodePayApp/CodePayApp/PhoneValidator.swift
+++ CodePayApp/CodePayApp/PhoneValidator.swift
@@ -8,21 +8,28 @@ class PhoneValidator {
                                "00 370 6xx xxxxx"
     ]
     
-    lazy private var prefixes: [String] = {
+    lazy private var compactMasks: [String] = {
+        var masks: [String] = []
+        validMasks.forEach {
+            let compactMask = $0.replacingOccurrences(of: " ", with: "")
+            masks.append(compactMask)
+        }
+        return masks
+    }()
+    
+    lazy private var maskPrefixes: [String] = {
         var prefixes: [String] = []
-        for mask in validMasks {
-            let compactMask = mask.replacingOccurrences(of: " ", with: "")
-            let prefix = compactMask.replacingOccurrences(of: "x", with: "")
+        compactMasks.forEach {
+            let prefix = $0.replacingOccurrences(of: "x", with: "")
             prefixes.append(prefix)
         }
         return prefixes
     }()
     
     func isValid(number: String) -> Bool {
-        for mask in validMasks {
-            let compactMask = mask.replacingOccurrences(of: " ", with: "")
-            let prefix = compactMask.replacingOccurrences(of: "x", with: "")
-            if number.count == compactMask.count && number.hasPrefix(prefix) {
+        for (index, mask) in compactMasks.enumerated() {
+            if number.count == mask.count &&
+                number.hasPrefix(maskPrefixes[index]) {
                 return true
             }
         }
@@ -31,19 +38,24 @@ class PhoneValidator {
     
     func validationNote(number: String) -> String {
         guard !isValid(number: number) else {
-            return "Valid number"
+            return "Number is Valid"
         }
         guard !number.isEmpty else {
             return ""
         }
         
-        for prefix in prefixes {
-            if number.count < prefix.count {
-                
+        for (index, maskPrefix) in maskPrefixes.enumerated() {
+            if number.count <= maskPrefix.count &&
+                number == maskPrefix.prefix(number.count) {
+                return compactMasks[index]
             }
             
+            if number.hasPrefix(maskPrefix) &&
+                number.count <= compactMasks[index].count {
+                let maskSuffix = compactMasks[index].dropFirst(number.count)
+                return number + maskSuffix
+            }
         }
-        
-        return "TestNote"
+        return "Invalid number"
     }
 }
