22a96a68eaf0ee011ac30e5f85c59a1990479118
TonyJem <a.jemanov@gmail.com>
2021-05-15 06:35:05 +0300

    Create SplashViewController and RootViewController

---
+++ CodePayApp/CodePayApp/ViewControllers/RootViewController.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    private var current: UIViewController
+    
+    init() {
+        self.current = SplashViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+        print("🟢 viewDidLoad")
+        
+        addChild(current)
+        current.view.frame = view.bounds
+        view.addSubview(current.view)
+        current.didMove(toParent: self)
+    }
+    
+    func showLoginScreen() {
+        let new = UINavigationController(rootViewController: LoginViewController())
+        
+        addChild(new)
+        new.view.frame = view.bounds
+        view.addSubview(new.view)
+        new.didMove(toParent: self)
+        
+        current.willMove(toParent: nil)
+        current.view.removeFromSuperview()
+        current.removeFromParent()
+        current = new
+    }
+    
+    func switchToMainScreen() {
+        let mainViewController = MainViewController()
+        let mainScreen = UINavigationController(rootViewController: mainViewController)
+        animateFadeTransition(to: mainScreen)
+    }
+    
+    private func animateFadeTransition(to new: UIViewController, completion: (() -> Void)? = nil) {
+        current.willMove(toParent: nil)
+        addChild(new)
+        transition(from: current, to: new, duration: 2, options: [.transitionCrossDissolve, .curveEaseOut], animations: {
+        }) { completed in
+            self.current.removeFromParent()
+            new.didMove(toParent: self)
+            self.current = new
+            completion?()
+        }
+    }
+    
+    func switchToLogout() {
+        let loginViewController = LoginViewController()
+        let logoutScreen = UINavigationController(rootViewController: loginViewController)
+        animateDismissTransition(to: logoutScreen)
+    }
+    
+    private func animateDismissTransition(to new: UIViewController, completion: (() -> Void)? = nil) {
+        current.willMove(toParent: nil)
+        addChild(new)
+        transition(from: current, to: new, duration: 2, options: [], animations: {
+            new.view.frame = self.view.bounds
+        }) { completed in
+            self.current.removeFromParent()
+            new.didMove(toParent: self)
+            self.current = new
+            completion?()
+        }
+    }
+}
